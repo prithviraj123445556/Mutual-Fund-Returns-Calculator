8dc85e1cf3a2b436bd0b0c1f0e5ff718615f1412
prithviraj123445556 <107459378+prithviraj123445556@users.noreply.github.com>
2023-12-05 21:09:32 +0530

    Update exceptions.py

--- src/app/exceptions.py
+++ src/app/exceptions.py
@@ -1,23 +1,26 @@
 from fastapi import HTTPException
 from starlette import status
 
-
+# Custom exception for Scheme Code Not Found
 class SchemeCodeNotFound(HTTPException):
     def __init__(self):
         super().__init__(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Scheme code not found",)
-
-class InvalidDate(HTTPException):
-    def __init__(self,detail="Invalid date"):
-        super().__init__(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Invalid date",
+            status_code=status.HTTP_404_NOT_FOUND,  # HTTP 404 status code for resource not found
+            detail="Scheme code not found",  # Error message for this exception
         )
 
+# Custom exception for Invalid Date
+class InvalidDate(HTTPException):
+    def __init__(self, detail="Invalid date"):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,  # HTTP 400 status code for bad request
+            detail="Invalid date",  # Error message for this exception
+        )
+
+# Custom exception for Invalid Capital Amount
 class InvalidCapitalAmount(HTTPException):
     def __init__(self):
         super().__init__(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Invalid capital amount. Please provide a valid number.",
+            status_code=status.HTTP_400_BAD_REQUEST,  # HTTP 400 status code for bad request
+            detail="Invalid capital amount. Please provide a valid number.",  # Error message for this exception
         )
